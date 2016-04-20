176b54f96a79febb1e45b6ce05cd468b35f072ab
FELIPE GOUVEA DA SILVA CARVALHO <95felipeg@gmail.com>
2016-04-20 18:29:30 -0300

    Updated

--- arvore-avl/main.c
+++ arvore-avl/main.c
@@ -1,7 +1,7 @@
         /*
             Felipe Gouvea, 92399
-               Algorítmo Arvore AVL com diversas funções porém não uma TAD
-            */
+            Algorítmo Arvore AVL com funções de inicialização, inserção, remoção e pesquisa.
+        */
 
     #include <stdio.h>
     #include <stdlib.h>
@@ -68,8 +68,15 @@ void imprime (TArvBin arv){
 
 void LL(TArvBin *A){
     TArvBin B =  (*A)->esq;
-    B->fb = 0;
-    (*A)->fb = 0;
+    if(B->fb == 0){
+        (*A)->fb =1;
+        B->fb = -1;
+    }
+    else{
+        (*A)->fb = 0;
+        B->fb = 0;
+    }
+
 
     (*A)->esq = B->dir;
     B->dir = (*A);
@@ -78,8 +85,14 @@ void LL(TArvBin *A){
 void RR(TArvBin *A){
     TArvBin B =  (*A)->dir;
 
-    B->fb = 0;
-    (*A)->fb = 0;
+    if(B->fb == 0){
+        (*A)->fb = -1;
+        B->fb = 1;
+    }
+    else{
+        (*A)->fb = 0;
+        B->fb = 0;
+    }
 
     (*A)->dir = B->esq;
     B->esq = (*A);
@@ -152,7 +165,7 @@ int Balanca_No(TArvBin *A){
             //printf("LR\n");
             LR(&(*A));
         }
-		
+
     }
     if (fb < -1){
         if (fb_dir <= 0){
@@ -193,9 +206,22 @@ int InsereAVL (TArvBin *aux, TItem novo){
 
     if (novo.Chave < (*aux)->Item.Chave) {
             aumentou = InsereAVL(&(*aux)->esq, novo);
+            //printf("fb %d\n", (*aux)->fb);
             if (aumentou == TRUE){
                 //printf("aumentou esq\n");
-                return AtualizaFB(&(*aux), DIR);
+                    if((*aux)->fb == -1){
+						(*aux)->fb = 0;
+						return 0;
+                    }
+
+                    if((*aux)->fb == 0){
+						(*aux)->fb = 1;
+						return 1;
+                    }
+                    if((*aux)->fb == 1){
+						return !BalancaEsq(&(*aux));
+                    }
+
             }
            else return FALSE;
            ///return (*aux)->fb;
@@ -205,9 +231,21 @@ int InsereAVL (TArvBin *aux, TItem novo){
     else {
         if (novo.Chave > (*aux)->Item.Chave) {
             aumentou = InsereAVL(&(*aux)->dir, novo);
+            //printf("fb %d\n", (*aux)->fb);
             if (aumentou == TRUE){
                 //printf("aumentou dir\n");
-                return AtualizaFB(&(*aux), ESQ);
+                    if((*aux)->fb == 1){
+						(*aux)->fb = 0;
+						return 0;
+                    }
+					if((*aux)->fb == 0){
+						(*aux)->fb = -1;
+						return 1;
+                    }
+					else if ((*aux)->fb == -1){
+						return !BalancaDir(&(*aux));
+                    }
+
             }
            else return FALSE;
         }
@@ -225,7 +263,7 @@ int DeletaNo(TArvBin *arv, TItem item){
         //printf("%d\n", (*arv)->Item.Chave);
         if(qtd_filhos(*arv) < 2){
             if((*arv)->dir == NULL){
-				//retira esq
+
                 aux = (*arv);
                 (*arv) = (*arv)->esq;
                 free(aux);
@@ -244,12 +282,11 @@ int DeletaNo(TArvBin *arv, TItem item){
             diminuiu = Substituto(&(*arv), &(*arv)->dir);
 			//printf("diminuiu %d", diminuiu);
 			//printf("FB = %d  NO = %d", (*arv)->fb, (*arv)->Item.Chave );
-			
+
             if(diminuiu == TRUE){
 
 				if((*arv)->fb == 1) {
-						(*arv)->fb++;
-						return Balanca_No(&(*arv));
+                    return BalancaEsq(&(*arv));
 				}
 				if((*arv)->fb == 0){
 					(*arv)->fb = 1;
@@ -257,7 +294,6 @@ int DeletaNo(TArvBin *arv, TItem item){
 
 				}
 				if((*arv)->fb == -1){
-
 					(*arv)->fb = 0;
 					return 1;
 				}
@@ -270,14 +306,13 @@ int Substituto(TArvBin *arv, TArvBin *arv_dir){
 		int prox = Substituto(&(*arv), &(*arv_dir)->esq);
 		if (prox == TRUE){
 				if((*arv_dir)->fb == -1) {
-					return Balanca_No(&(*arv_dir)->dir);
+					return BalancaDir(&(*arv_dir));
 				}
 				if((*arv_dir)->fb == 0){
 					(*arv_dir)->fb = -1;
 					return 0;
 				}
 				if((*arv_dir)->fb == 1){
-
 					(*arv_dir)->fb = 0;
 					return 1;
 				}
@@ -296,44 +331,56 @@ int Substituto(TArvBin *arv, TArvBin *arv_dir){
 int RemoveAVL (TArvBin *aux, TItem item){
     //printf("removendo...\n");
     if((*aux) == NULL) return 0;
+	else if (item.Chave < (*aux)->Item.Chave){
+        int result = RemoveAVL(&(*aux)->esq, item);
+		if(result == TRUE){
+				if((*aux)->fb == 1){
+					(*aux)->fb = 0;
+					return 1;
+					}
+				if((*aux)->fb == 0){
+					(*aux)->fb = -1;
+					return 0;
+					}
+                if((*aux)->fb == -1){
+					return BalancaDir(&(*aux));
+                }
+		}
+		else return FALSE;
+	}
+	else if (item.Chave > (*aux)->Item.Chave){
+		if(RemoveAVL(&(*aux)->dir, item)){
+				if((*aux)->fb == -1){
+					(*aux)->fb = 0;
+					return 1;
+					}
+				if((*aux)->fb == 0){
+					(*aux)->fb = 1;
+					return 0;
+                }
+				else if ((*aux)->fb == 1){
+					return BalancaEsq(&(*aux));
+                }
 
-    if((*aux)->Item.Chave == item.Chave) {
-        return DeletaNo(&(*aux), item);
-        //printf("deletou!\n");
+		}
+		else return FALSE;
 
-    }
-    int diminuiu;
-    if(item.Chave > (*aux)->Item.Chave){
-        diminuiu = RemoveAVL(&(*aux)->dir, item);
-        if(diminuiu == TRUE){
-            return !AtualizaFB(&(*aux), DIR);
-        }
-        else {
-            return FALSE;
-        }
-    }
-    else if(item.Chave < (*aux)->Item.Chave){
-        diminuiu = RemoveAVL(&(*aux)->esq, item);
-        if(diminuiu == TRUE){
-            return !AtualizaFB(&(*aux), DIR);
-        }
-        else {
-            return FALSE;
-        }
-    }
-
+	}
+	else {
+		return DeletaNo(&(*aux), item);
+	}
 }
 
 int PesquisaABB(TArvBin *arv, TItem item){
     //printf("%d ", (*arv)->Item.Chave);
     if(*arv == NULL) {
         //printf("Nao encontrou\n");
-        InsereAVL(&(*arv), item);
+        //InsereAVL(&(*arv), item);
         return NAO_CONTEM;
     }
     if((*arv)->Item.Chave == item.Chave){
         //printf("Encontrou a chave!\n");
-        RemoveAVL(&(*arv), item);
+        //RemoveAVL(&(*arv), item);
         return CONTEM;
     }
     if(item.Chave > (*arv)->Item.Chave){
@@ -354,54 +401,35 @@ void ImprimeFB(TArvBin arv){
      ImprimeFB(arv->esq);
      ImprimeFB(arv->dir);
 }
-AtualizaFB(TArvBin *aux, int direction){
-
-    if (direction == DIR){
-        return AtualizaEsquerda(&(*aux));
-    }
-    else if (direction == ESQ){
-        return AtualizaDireita(&(*aux));
-         }
-
+int BalancaEsq(TArvBin *aux){
+	int fb = Fator_Balanceamento((*aux)->esq);
+	if(fb > 0){
+		LL(&(*aux));
+		return 1;
+	}
+	if(fb < 0){
+		LR(&(*aux));
+		return 1;
+	}
+	else {
+		LL(&(*aux));
+		return 0;
+	}
 }
-int AtualizaEsquerda(TArvBin *aux){
-        if((*aux)->fb >= 1){
-            //printf("ESQ 1\n");
-            (*aux)->fb++;
-            //return 1;
-            return Balanca_No(&(*aux));
-            //BALANÇAESQUERDA
-        }
-        if((*aux)->fb == 0){
-            //printf("ESQ 0\n");
-            (*aux)->fb = 1;
-            return 1;
-        }
-        if((*aux)->fb == -1){
-            //printf("ESQ -1\n");
-            (*aux)->fb = 0;
-            return 0;
-        }
-
-}
-int AtualizaDireita(TArvBin *aux){
-            if((*aux)->fb == 1){
-                //printf("DIR 1\n");
-                (*aux)->fb = 0;
-                return 0;
-            }
-            if((*aux)->fb == 0){
-                //printf("DIR 0\n");
-                (*aux)->fb = -1;
-                return 1;
-            }
-            if((*aux)->fb <= -1){
-                //printf("DIR -1\n");
-                (*aux)->fb--;
-                //return 1;
-                return Balanca_No(&(*aux));
-            //BALANÇADIREITA
-            }
+int BalancaDir(TArvBin *aux){
+	int fb = Fator_Balanceamento((*aux)->dir);
+	if(fb > 0){
+		RL(&(*aux));
+		return 1;
+	}
+	if(fb < 0){
+		RR(&(*aux));
+		return 1;
+	}
+	else {
+		LL(&(*aux));
+		return 0;
+	}
 }
 int main(){
         TArvBin td;
@@ -415,14 +443,27 @@ int main(){
             scanf("%d", &novo.Chave);
             InsereAVL(&avl, novo);
             entrada--;
-
+            //printf("\n");
+            ImprimeFB(avl);
+            imprime(avl);
             //Balanca_Arv(&avl);
-            //imprime(avl);
+
         }
         //imprime(avl);
+        //ImprimeFB(avl);
+
         scanf("%d", &novo.Chave);
         //printf("%d\n", QuantidadeNos(abb));
-        PesquisaABB(&avl, novo);
+        int result = PesquisaABB(&avl, novo);
+        if(result ==CONTEM){
+            RemoveAVL(&avl, novo);
+            //printf("REMOVEU\n");
+        }
+        else if(result == NAO_CONTEM){
+            InsereAVL(&avl, novo);
+            //printf("INSERIU\n");
+        }
+
         //printf("\n");
         //ImprimeFB(avl);
         imprime(avl);
