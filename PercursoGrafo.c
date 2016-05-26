ae94da5ad31ad742918f3a30c67ca19037bb0970
lammer <95felipeg@gmail.com>
2016-05-26 14:17:16 -0300

    Grafo pronto

--- PercursoGrafo.c
+++ PercursoGrafo.c
@@ -23,7 +23,7 @@ typedef struct{
 
 typedef struct SGrafo *TGrafo;
 typedef struct SGrafo{
-    TLista *Inicio;
+    TLista *lista;
     TGrafo ProxNo;
     int codigo;
 }STGrafo;
@@ -32,13 +32,21 @@ typedef struct SGrafo{
 
 void Inicializa(TGrafo *G, int QuantidadeNos){
     int i;
-    //(*G)  = (TGrafo)malloc(sizeof(STGrafo));
-    for(i = 0; i< QuantidadeNos; i++){
-        printf("i : %d", i);
-        (*G)->codigo = i;
-        (*G)->ProxNo = (TGrafo)malloc(sizeof(STGrafo));
-    }
+    (*G)  = (TGrafo)malloc(sizeof(STGrafo));
+    TGrafo aux = (*G);
+    if (aux == NULL) printf("NULO");
+    for(i = 1; i<= QuantidadeNos; i++){
+        if((aux)->ProxNo == NULL){
+            //printf("nulo");
+            (aux)->ProxNo = (TGrafo)malloc(sizeof(STGrafo));
+        }
+        aux->lista = (TLista*)malloc(sizeof(TLista));
+        InicializaLista(aux->lista);
 
+        (aux)->codigo = i;
+        (aux) = (aux)->ProxNo;
+
+    }
 }
 
 void InicializaLista(TLista *lista){
@@ -48,37 +56,95 @@ void InicializaLista(TLista *lista){
 void Imprime(TGrafo G){
 
     TGrafo aux = G;
-    while (aux != NULL){
-        printf("%d", aux->codigo);
+    TApontador aux2;
+    while (aux->ProxNo != NULL){
+        aux2 = aux->lista->Inicio;
+        printf("[%d]: ", aux->codigo);
+        while(aux2 != NULL){
+            printf("%d ", aux2->Item.Chave);
+            aux2 = aux2->prox;
+        }
+        printf("\n");
         aux = aux->ProxNo;
     }
 }
-int main(void){
-        int quantidadeNos;
-        int quantidadeVertices;
-        //scanf("%d", &quantidadeNos);
-        //scanf("%d", &quantidadeVertices);
-        int cont,num;
-        TGrafo G;
-        G = NULL;
-        Inicializa(&G, 5);
-        //if(G == NULL) printf("G NULO");
-        Imprime(G);
-        //Inicializa(&G, quantidadeNos);
-        //Imprime(tab);
-        /*
-        for(cont = 0; cont < QuantidadeVertices ; cont++){
-            scanf("%d", &num);
-            InsereGrafo(&tab,num);
+void InsereGrafo(TGrafo *T, int origem, int destino, int tipoRua){
+    if(*T == NULL) return;
+    else{
+        if(tipoRua == 1){
+            //printf("Tipo 1     ");
+            InsereRecursivo(T, origem, destino);
         }
-        //printf("%d",tab.Lista[num % tab.tam].Inicio->Item.Chave);
-        scanf("%d", &num);
-        if(PesquisaHash(&tab, num) == ENCONTROU){
-            RemoveHash(&tab,num);
+        if(tipoRua == 2){
+           //printf("Tipo 2     ");
+            InsereRecursivo(T, origem, destino);
+            InsereRecursivo(T, destino, origem);
         }
-        else InsereHash(&tab,num);
-        Imprime(&tab);
-        //printf("%d", tab->Item[0].Chave);
-        */
-        return 0;
     }
+}
+void InsereRecursivo(TGrafo *T, int origem, int destino){
+    TGrafo aux = (*T);
+    while (aux != NULL){
+        if((aux)->codigo == origem){
+            //printf("Vertice %d", aux->codigo);
+            InsereLista((aux)->lista, destino);
+            //printf("Inseriu\n");
+            return;
+        }
+        //printf("Erro ");
+        aux = aux->ProxNo;
+    }
+}
+void InsereLista(TLista *l, int info){
+    TApontador aux = (*l).Fim;
+    if(l == NULL)printf("Lista Nula");
+    if(aux == NULL){
+        TApontador novo = (TApontador)malloc(sizeof(TNo));
+        novo->Item.Chave = info;
+        novo->prox = NULL;
+
+        aux = novo;
+        l->Inicio = aux;
+        l->Fim = aux;
+
+        //printf("Inseriu na lista %d\n", info);
+    }
+    else{
+        TApontador novo = (TApontador)malloc(sizeof(TNo));
+        novo->Item.Chave = info;
+        novo->prox = NULL;
+
+        l->Fim->prox = novo;
+        l->Fim = novo;
+    }
+}
+
+void BuscaLargua(){
+
+}
+
+int main(void){
+    int quantidadeNos;
+    int quantidadeArestas;
+    scanf("%d", &quantidadeNos);
+    scanf("%d", &quantidadeArestas);
+    int cont;
+    int origem;
+    int destino;
+    int tipoRua;
+    TGrafo G;
+    G = NULL;
+    Inicializa(&G, quantidadeNos);
+
+
+
+    for(cont = 0; cont < quantidadeArestas ; cont++){
+        scanf("%d", &origem);
+        scanf("%d", &destino);
+        scanf("%d", &tipoRua);
+        InsereGrafo(&G, origem, destino, tipoRua);
+    }
+    printf("\n\nImpressao:\n");
+    Imprime(G);
+    return 0;
+}
